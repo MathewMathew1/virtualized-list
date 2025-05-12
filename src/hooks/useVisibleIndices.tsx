f7ee7c06e0d2ef0062f9a9ce1a7c53377afbf996
Mathew <mateusz.lu123@gmail.com>
2025-05-12 17:01:52 +0200

    removed unneded lines

--- src/hooks/useVisibleIndices.tsx
+++ src/hooks/useVisibleIndices.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import { getSize } from "../helpers/size";
+import { useEffect, useRef, useMemo, useState } from "react";
 import { useTotalSize } from "./useTotalSize";
 import { searchStartingIndex } from "../helpers/search";
+import { getSize } from "../helpers/size";
 
 export function useVisibleIndices(
   dataLength: number,
@@ -9,71 +9,94 @@ export function useVisibleIndices(
   scrollOffset: number,
   viewportSize: number,
   buffer: number,
-  padding: number
+  padding: number,
 ) {
   const [visible, setVisible] = useState({ firstVisible: 0, lastVisible: 0 });
   const { total, setValuesSinceIndex, sizesOffsetOfIndice, sizesSet } =
     useTotalSize(itemSize, dataLength);
 
+  const lastFirstIndice = useRef(0);
+
+  const rafRef = useRef<number | null>(null);
+
+  const viewportSizeWithPadding = useMemo(
+    () => viewportSize - padding,
+    [viewportSize, padding],
+  );
+
   useEffect(() => {
-    const viewportSizeWithPadding = viewportSize - padding;
-
-    if (Array.isArray(itemSize) || typeof itemSize === "function") {
-      if (!sizesSet) return;
-      const startingIndex = searchStartingIndex(
-        sizesOffsetOfIndice,
-        0,
-        0,
-        dataLength - 1,
-        scrollOffset
-      );
-
-      let currentOffset = sizesOffsetOfIndice[startingIndex] || 0,
-        end = startingIndex;
-
-      for (let i = startingIndex; i < dataLength; i++) {
-        if (currentOffset < scrollOffset + viewportSizeWithPadding) {
-          end = i;
-        } else {
-          break;
-        }
-        currentOffset += getSize(itemSize, i);
-      }
-
-      const first = Math.max(startingIndex - buffer, 0);
-      const last = Math.min(end + buffer, dataLength - 1);
-      setVisible((prev) => {
-        if (prev.firstVisible !== first || prev.lastVisible !== last) {
-          return { firstVisible: first, lastVisible: last };
-        }
-        return prev;
-      });
-    } else {
-      const first = Math.max(Math.floor(scrollOffset / itemSize) - buffer, 0);
-
-      const last = Math.min(
-        Math.ceil((scrollOffset + viewportSizeWithPadding) / itemSize) +
-          buffer -
-          1,
-        dataLength - 1
-      );
-
-      setVisible((prev) => {
-        if (prev.firstVisible !== first || prev.lastVisible !== last) {
-          return { firstVisible: first, lastVisible: last };
-        }
-        return prev;
-      });
+    if (rafRef.current) {
+      cancelAnimationFrame(rafRef.current);
     }
+
+    rafRef.current = requestAnimationFrame(() => {
+      if (Array.isArray(itemSize) || typeof itemSize === "function") {
+        if (!sizesSet) return;
+
+        const startingIndex = searchStartingIndex(
+          sizesOffsetOfIndice,
+          lastFirstIndice.current,
+          0,
+          dataLength - 1,
+          scrollOffset,
+        );
+
+        lastFirstIndice.current = startingIndex;
+
+        let currentOffset = sizesOffsetOfIndice[startingIndex] || 0;
+        let end = startingIndex;
+
+        for (let i = startingIndex; i < dataLength; i++) {
+          if (currentOffset < scrollOffset + viewportSizeWithPadding) {
+            end = i;
+          } else {
+            break;
+          }
+          currentOffset += getSize(itemSize, i);
+        }
+
+        const first = Math.max(startingIndex - buffer, 0);
+        const last = Math.min(end + buffer, dataLength - 1);
+
+        setVisible((prev) => {
+          if (visible.firstVisible !== first || visible.lastVisible !== last)
+            return { firstVisible: first, lastVisible: last };
+          return prev;
+        });
+      } else {
+        const first = Math.max(Math.floor(scrollOffset / itemSize) - buffer, 0);
+
+        const last = Math.min(
+          Math.ceil((scrollOffset + viewportSizeWithPadding) / itemSize) +
+            buffer -
+            1,
+          dataLength - 1,
+        );
+
+        setVisible((prev) => {
+          if (visible.firstVisible !== first || visible.lastVisible !== last)
+            return { firstVisible: first, lastVisible: last };
+          return prev;
+        });
+      }
+    });
+
+    return () => {
+      if (rafRef.current) {
+        cancelAnimationFrame(rafRef.current);
+      }
+    };
   }, [
     scrollOffset,
     itemSize,
-    viewportSize,
+    viewportSizeWithPadding,
     buffer,
     dataLength,
     sizesOffsetOfIndice,
     sizesSet,
+    visible.firstVisible,
+    visible.lastVisible,
   ]);
- 
-  return {visible, total, setValuesSinceIndex, sizesOffsetOfIndice: sizesOffsetOfIndice}
+
+  return { visible, total, setValuesSinceIndex, sizesOffsetOfIndice };
 }
